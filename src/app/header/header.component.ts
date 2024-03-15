3ecbc86b8bf9d846a333be46383f16b65c45cba6
Amit-limbasiya <amit.limbasiya@marutitech.com>
2024-03-15 18:51:12 +0530

    http module implemented

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,16 @@
 import { Component, EventEmitter, Output } from "@angular/core";
+import { DataStorageService } from "../shared/data-storage.service";
 @Component({
     selector:"app-header",
     templateUrl: "./header.component.html"
 })
 export class HeaderComponent{
-    @Output() navigate = new EventEmitter<string>();
-    SelectedFeature(feature:string){
-        this.navigate.emit(feature);
+    constructor(private dataStorageService:DataStorageService) {}
+
+    onSaveData(){
+        this.dataStorageService.storeRecipe();
+    }
+    onFetchData(){
+        this.dataStorageService.fetchRecipes().subscribe();
     }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,4 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import { EventEmitter, Injectable, OnInit } from "@angular/core";
 import { Recipe } from "./recipe.model";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
@@ -6,17 +6,21 @@ import { Subject } from "rxjs";
 
 @Injectable()
 export class RecipeService{
+    // private recipes: Recipe[] = [
+    //     new Recipe("Tasty Burger","This is the burger!","https://img.freepik.com/free-photo/delicious-burger-with-many-ingredients-isolated-white-background-tasty-cheeseburger-splash-sauce_90220-1266.jpg?w=1380&t=st=1710249618~exp=1710250218~hmac=e5d8b271fe809fcc1190d0578367f80d7862b60cc33c7109ce1710ede97baed2",
+    //         [{name:"a",amount:5},{name:"b",amount:15}]),
+    //     new Recipe("Pizza","What else you want to know!","https://img.freepik.com/free-photo/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_141793-2158.jpg",
+    //         [{name:"c",amount:25},{name:"d",amount:45}])
+    // ];
+    private recipes: Recipe[] = [];
     onRecipeChanged = new Subject<Recipe[]>();
-    constructor(private slService:ShoppingListService) {
-        
-    }
-    private recipes: Recipe[] = [
-        new Recipe("Tasty Burger","This is the burger!","https://img.freepik.com/free-photo/delicious-burger-with-many-ingredients-isolated-white-background-tasty-cheeseburger-splash-sauce_90220-1266.jpg?w=1380&t=st=1710249618~exp=1710250218~hmac=e5d8b271fe809fcc1190d0578367f80d7862b60cc33c7109ce1710ede97baed2",
-            [{name:"a",amount:5},{name:"b",amount:15}]),
-        new Recipe("Pizza","What else you want to know!","https://img.freepik.com/free-photo/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_141793-2158.jpg",
-            [{name:"c",amount:25},{name:"d",amount:45}])
-    ];
 
+    constructor(private slService:ShoppingListService) {}
+
+    setRecipes(recipes:Recipe[]){
+        this.recipes=recipes;
+        this.onRecipeChanged.next(this.recipes.slice());
+    }
     getRecipes(){
         return this.recipes.slice();
     }
